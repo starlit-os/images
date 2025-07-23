5078b7bad486d3a3cd71bf50bc42cc2431d68e20
Lily <20535390+kitten-lily@users.noreply.github.com>
2025-07-23 09:25:42 +0200

    Adds Discord and Ghostty
    Adds a script to install Discord and Ghostty.
    
    Enables RPM Fusion and Ghostty COPR, installs the packages, and then disables them.
    
    Also enables podman socket, podman-restart service, and podman-auto-update timer.

--- Containerfile
+++ Containerfile
@@ -8,7 +8,7 @@ FROM ghcr.io/ublue-os/bazzite:stable@sha256:2e4a612571d9845db2d1d0fb783c26a67bfc
 ## Other possible base images include:
 # FROM ghcr.io/ublue-os/bazzite:latest
 # FROM ghcr.io/ublue-os/bluefin-nvidia:stable
-# 
+#
 # ... and so on, here are more base images
 # Universal Blue Images: https://github.com/orgs/ublue-os/packages
 # Fedora base image: quay.io/fedora/fedora-bootc:41
@@ -24,7 +24,7 @@ RUN --mount=type=bind,from=ctx,source=/,target=/ctx \
     --mount=type=tmpfs,dst=/tmp \
     /ctx/build.sh && \
     ostree container commit
-    
+
 ### LINTING
 ## Verify final image and contents are correct.
 RUN bootc container lint
--- build_files/build.sh
+++ build_files/build.sh
@@ -2,23 +2,28 @@
 
 set -ouex pipefail
 
+### Workaround for missing /var/roothome
+mkdir /var/roothome
+
 ### Install packages
 
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
+# Enable RPM Fusion repositories
+dnf5 config-manager setopt rpmfusion-free.enabled=1 rpmfusion-free-updates.enabled=1 \
+    rpmfusion-nonfree.enabled=1 rpmfusion-nonfree-updates.enabled=1
 
-# this installs a package from fedora repos
-dnf5 install -y tmux 
+# Enable Ghostty copr
+dnf5 copr enable -y scottames/ghostty
 
-# Use a COPR Example:
-#
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
-# Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
+dnf5 install -y discord podman-docker ghostty
 
-#### Example for enabling a System Unit File
+# Disable Ghostty copr
+dnf5 copr disable scottames/ghostty
 
+# Disable RPM Fusion repositories
+dnf5 config-manager setopt rpmfusion-free.enabled=0 rpmfusion-free-updates.enabled=0 \
+    rpmfusion-nonfree.enabled=0 rpmfusion-nonfree-updates.enabled=0
+
+### Enable services
 systemctl enable podman.socket
+systemctl enable podman-restart.service
+systemctl enable podman-auto-update.timer
