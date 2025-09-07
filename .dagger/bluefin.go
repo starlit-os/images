46e327d7900a6a3a5345738c078bd0da4922d3dd
Lily <20535390+kitten-lily@users.noreply.github.com>
2025-09-07 13:48:02 +0200

    Add Cayo

---
+++ .dagger/bluefin.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"context"
+	"dagger/bazzite/internal/dagger"
+)
+
+// Creates a Bluefin container
+func (m *Bazzite) BluefinContainer(
+	ctx context.Context,
+	// +defaultPath="/"
+	source *dagger.Directory,
+	source_image string,
+) *Bazzite {
+	return m.From(ctx, source_image).
+		WithRpmfusion(ctx).
+		WithTerra(ctx).
+		WithReposEnabled(ctx, []string{"warpdotdev"}).
+		WithDirectory(ctx, "system_files/shared", "/").
+		WithDirectory(ctx, "system_files/desktop", "/").
+		WithPackages(ctx, []string{
+			"coolercontrol",
+			"headsetcontrol",
+			"liquidctl",
+			"openrgb",
+			"podman-docker",
+			"warp-terminal",
+		}).
+		WithOptFix(ctx, "warpdotdev", "warp-terminal", "warp-terminal/warp").
+		WithServices(ctx, []string{
+			"podman.socket",
+			"podman-restart.service",
+			"podman-auto-update.timer",
+		})
+}
---
+++ .dagger/cayo.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"context"
+	"dagger/bazzite/internal/dagger"
+)
+
+// Creates a Cayo container
+func (m *Bazzite) CayoContainer(
+	ctx context.Context,
+	// +defaultPath="/"
+	source *dagger.Directory,
+	source_image string,
+) *Bazzite {
+	return m.From(ctx, source_image).
+		WithDirectory(ctx, "system_files/shared", "/").
+		WithPackages(ctx, []string{
+			"podman-docker",
+		}).
+		WithServices(ctx, []string{
+			"podman.socket",
+			"podman-restart.service",
+			"podman-auto-update.timer",
+		})
+}
