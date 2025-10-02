2435fdddf5170b18023d9c1139964d179add80ef
Lily <20535390+kitten-lily@users.noreply.github.com>
2025-10-02 19:15:33 +0200

    Add Docker to Bazzite (#84)
    * Add Docker to Bazzite
    
    * Fix

--- .dagger/bazzite.go
+++ .dagger/bazzite.go
@@ -15,9 +15,11 @@ func (m *Bazzite) BazziteContainer(
 	return m.From(ctx, source_image).
 		WithRpmfusion(ctx).
 		WithTerra(ctx).
+		WithReposEnabled(ctx, []string{"docker-ce-stable"}).
 		WithReposEnabled(ctx, []string{"warpdotdev"}).
 		WithDirectory(ctx, "system_files/shared", "/").
 		WithDirectory(ctx, "system_files/desktop", "/").
+		WithDirectory(ctx, "system_files/bazzite", "/").
 		WithCopr(ctx, "scottames/ghostty").
 		WithCopr(ctx, "che/nerd-fonts").
 		WithPackages(ctx, []string{
@@ -28,13 +30,19 @@ func (m *Bazzite) BazziteContainer(
 			"liquidctl",
 			"nerd-fonts",
 			"openrgb",
-			"podman-docker",
+			"docker-ce",
+			"docker-ce-cli",
+			"docker-compose-plugin",
+			"docker-model-plugin",
 			"warp-terminal",
 		}).
 		WithOptFix(ctx, "warpdotdev", "warp-terminal", "warp-terminal/warp").
 		WithServices(ctx, []string{
+			"docker.service",
+			"docker.socket",
 			"podman.socket",
 			"podman-restart.service",
 			"podman-auto-update.timer",
+			"tailscaled.service",
 		})
 }
